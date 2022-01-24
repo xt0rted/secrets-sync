10d1d530fbe47e44797d0dcc838865c36628fd06
Brian Surowiec <brian@13degrees.com>
2022-01-24 16:08:38 -0500

    Initial code commit

---
+++ src/github.ts
@@ -0,0 +1,118 @@
+import {
+  debug,
+  getInput,
+  info,
+  warning,
+} from "@actions/core";
+import {
+  GitHub,
+  getOctokitOptions,
+} from "@actions/github/lib/utils";
+import memoize from "@github/memoize";
+import { retry } from "@octokit/plugin-retry";
+import { throttling } from "@octokit/plugin-throttling";
+import { RequestError } from "@octokit/request-error";
+
+/* eslint-disable import/order */
+import type { components } from "@octokit/openapi-types";
+import type { OctokitResponse } from "@octokit/types";
+/* eslint-enable import/order */
+
+const octokit = GitHub.plugin(
+  retry,
+  throttling,
+);
+const github = new octokit(
+  getOctokitOptions(
+    getInput("repo_token", { required: true }),
+    {
+      throttle: {
+        onRateLimit: () => true,
+        onAbuseLimit: () => true,
+      },
+    },
+  ),
+);
+
+export type SecretEnvironment =
+  | "actions"
+  | "dependabot"
+  ;
+
+interface DeleteSecretOptions {
+  environment: SecretEnvironment;
+  owner: string;
+  repo: string;
+  secret: string;
+}
+
+export async function deleteSecret({ environment, owner, repo, secret }: DeleteSecretOptions) {
+  info(`Deleting ${secret} in ${owner}/${repo}`);
+
+  try {
+    await github.request("DELETE /repos/{owner}/{repo}/{environment}/secrets/{secret_name}", {
+      environment,
+      owner,
+      repo,
+      secret_name: secret,
+    });
+  } catch (error_) {
+    const { status, message } = error_ as RequestError;
+
+    if (status === 404) {
+      warning(`Secret ${secret} does not exist in ${owner}/${repo}`);
+
+      return;
+    }
+
+    throw new Error(`Error deleting ${environment} secret ${secret} in ${owner}/${repo}; ${status} ${message}`);
+  }
+}
+
+interface SetSecretOptions {
+  environment: SecretEnvironment;
+  owner: string;
+  repo: string;
+  secret: string;
+  value: string;
+  keyId: string;
+}
+
+export async function setSecret({ environment, owner, repo, secret, value, keyId }: SetSecretOptions): Promise<void> {
+  info(`Setting ${secret} in ${owner}/${repo}`);
+
+  try {
+    await github.request("PUT /repos/{owner}/{repo}/{environment}/secrets/{secret_name}", {
+      environment,
+      owner,
+      repo,
+      secret_name: secret,
+      encrypted_value: value,
+      key_id: keyId,
+    });
+  } catch (error_) {
+    const { status, message } = error_ as RequestError;
+
+    throw new Error(`Error setting ${environment} secret ${secret} in ${owner}/${repo}; ${status} ${message}`);
+  }
+}
+
+type GitHubSecretKeyResponse = components["schemas"]["actions-public-key"];
+
+interface GetPublicKeyOptions {
+  environment: SecretEnvironment;
+  owner: string;
+  repo: string;
+}
+
+export const getPublicKey = memoize(async ({ environment, owner, repo }: GetPublicKeyOptions): Promise<GitHubSecretKeyResponse> => {
+  debug(`Getting ${environment} private key for ${owner}/${repo}`);
+
+  const result = await github.request("GET /repos/{owner}/{repo}/{environment}/secrets/public-key", {
+    environment,
+    owner,
+    repo,
+  }) as OctokitResponse<GitHubSecretKeyResponse, number>;
+
+  return result.data;
+});
